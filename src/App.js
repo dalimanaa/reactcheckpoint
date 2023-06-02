89fc0e1cc25120bbfa7de088fb7d18af8f129c3f
dalimanaa <121606249+dalimanaa@users.noreply.github.com>
2023-06-02 13:08:03 +0200

    react check point

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from 'react-bootstrap ; 
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    // form inputs
+    function TextControlsExample() {
+      return (
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="name@example.com" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Example textarea</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+        </Form>
+      );
+    }
+    
+  );
+}
+
+export default App;
+
+export default TextControlsExample; 
